371110b5fea545ff8637681808ad32853413e8e1
Miqueas <miqueasgigena18@gmail.com>
2025-07-17 15:53:22 -0300

    Final Lesson of currency exchange

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,45 @@
 import { useState, useEffect } from 'react'
-import Note from './components/Note'
-import noteService from './services/notes'
-import Notification from './components/Notification'
-import Footer from './components/Footer'
-import './index.css' 
+import axios from 'axios'
 
 const App = () => {
-  const [notes, setNotes] = useState(null)
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true) 
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [value, setValue] = useState('')
+  const [rates, setRates] = useState({})
+  const [currency, setCurrency] = useState(null)
 
   useEffect(() => {
-    console.log('effect')
-    noteService.getAll().then(initialNotes => {
-        setNotes(initialNotes)
-      })
-  }, [])
-    
-  // do not render anything if notes is still null
-  if(!notes) {
-    return null
-  }
+    console.log('effect run, currency is now', currency)
 
-  console.log('render', notes.length, 'notes')
-
-  const toggleImportanceOf = id => {
-  const note = notes.find(n => n.id === id)
-  const changedNote = { ...note, important: !note.important }
-  console.log("found note", note)
-  console.log("changed Note", changedNote)
-
-  noteService.update(id, changedNote).then(returnedNote => {
-      setNotes(notes.map(note => note.id === id ? returnedNote : note))
-    })  
-    .catch(() => {
-      setErrorMessage(`Note '${note.content}' was already removed from server`)
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
-    })
-}
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
+    // skip if currency is not defined
+    if (currency) {
+      console.log('fetching exchange rates...')
+      axios
+        .get(`https://open.er-api.com/v6/latest/${currency}`)
+        .then(response => {
+          setRates(response.data.rates)
+        })
     }
+  }, [currency])
 
-    noteService.create(noteObject).then(returnedNote => {
-      setNotes(notes.concat(returnedNote))
-      setNewNote('')
-    })
-  }
-  
-  const handleNoteChange = (event) => {
-      console.log(event.target.value)
-      setNewNote(event.target.value)
+  const handleChange = (event) => {
+    setValue(event.target.value)
   }
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+  const onSearch = (event) => {
+    event.preventDefault()
+    setCurrency(value)
+  }
 
-
-
-    return (
-      <div>
-        <h1>Notes</h1>
-        <Notification message={errorMessage} />
-        <div>
-            <button onClick={() => setShowAll(!showAll)}>
-              show {showAll ? 'important' : 'all'}
-            </button>
-        </div>
-        <ul>
-          {notesToShow.map(note => 
-            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
-          )}
-        </ul>
-        <form onSubmit={addNote}>
-          <input
-            value={newNote}
-            onChange={handleNoteChange}
-          />
-          <button type="submit">save</button>
-        </form>   
-        <Footer />
-      </div>
-    )
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        currency: <input value={value} onChange={handleChange} />
+        <button type="submit">exchange rate</button>
+      </form>
+      <pre>
+        {JSON.stringify(rates, null, 2)}
+      </pre>
+    </div>
+  )
 }
 
-export default App 
+export default App
